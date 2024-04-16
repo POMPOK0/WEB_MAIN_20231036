d3a5a17fb37786e1f413e3b3c248ae452bb0e354
admin <eden20041228@gmail.com>
2024-04-17 00:08:55 +0900

    5주차 최종

---
+++ js/pop_up.js
@@ -0,0 +1,4 @@
+function pop_up()
+{
+    window.open("C:/Users/eden2/Documents/학교/자바/WEB_MAIN_20231036/popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+}
--- js/serch.js
+++ js/serch.js
@@ -1,5 +1,8 @@
-document.getElementById("serch_btn").addEventListener('click', search_message);
-function search_message()
-{
-    alert("검색을 수행합니다!");
+document.getElementById("serch_btn").addEventListener('click', googleSearch);
+
+function googleSearch() {
+    const searchTerm = document.getElementById("Search_input").value;
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    window.open(googleSearchUrl, "_blank");
+    return false;
 }

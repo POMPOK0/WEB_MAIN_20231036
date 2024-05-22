c6e97d7e9bc1dccac730e97999fc1ecb27405926
admin <eden20041228@gmail.com>
2024-05-22 14:25:40 +0900

    12주차

--- js/login.js
+++ js/login.js
@@ -60,6 +60,9 @@ const check_input = () =>
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
 
+    
+        
+
     if (emailValue === '') 
     {
         alert('이메일을 입력하세요.');
@@ -132,11 +135,9 @@ const check_input = () =>
     return false;
     }
 
-
-        
-
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
+    session_set();
     loginForm.submit();
 
 
@@ -145,3 +146,128 @@ const check_input = () =>
 };
 
     document.getElementById("login_btn").addEventListener('click', check_input);
+
+function session_set()
+{
+    let session_id = document.querySelector("#typeEmailX");
+    let session_pass = document.querySelector("#typePasswordX");
+    if (sessionStorage)
+    {
+        let en_text = encrypt_text(session_pass.value);
+        sessionStorage.setItem("Session_Storage_id", session_id.value);
+        sessionStorage.setItem("Session_Storage_pass", en_text);
+    }
+    else 
+    {
+        alert("로컬 스토리지 지원 X");
+    }
+}
+
+function session_get()
+{
+    if (sessionStorage) 
+    {
+        return sessionStorage.getItem("Session_Storage_pass");
+    }
+    else
+    {
+        alert("세션 스토리지 지원 X");
+    }
+}
+
+function session_check() 
+{
+    if (sessionStorage.getItem("Session_Storage_id")) 
+    {
+        alert("이미 로그인 되었습니다.");
+        location.href='../login/index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+
+function session_del() 
+{
+    if (sessionStorage) 
+    {
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } 
+    else 
+    {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function session_del() 
+{//세션 삭제
+    if (sessionStorage) 
+    {
+    sessionStorage.removeItem("Session_Storage_test");
+    alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } 
+    else 
+    {
+        alert("세션 스토리지 지원 x");
+    }
+}
+    
+
+function logout()
+{
+    session_del(); // 세션 삭제
+    location.href='../index.html';
+}
+
+function encodeByAES256(key, data)
+{
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), 
+    {
+    iv: CryptoJS.enc.Utf8.parse(""),
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+}
+
+function decodeByAES256(key, data)
+{
+    const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), 
+    {
+    iv: CryptoJS.enc.Utf8.parse(""),
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+}
+
+function encrypt_text(password)
+{
+    const k = "key"; // 클라이언트 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const b = password;
+    const eb = this.encodeByAES256(rk, b);
+    return eb;
+    console.log(eb);
+}
+
+function decrypt_text()
+{
+    const k = "key"; // 서버의 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const eb = session_get();
+    const b = this.decodeByAES256(rk, eb);
+    console.log(b);
+}
+
+function init_logined()
+{
+    if(sessionStorage)
+    {
+        decrypt_text(); // 복호화 함수
+    }
+    else
+    {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+    
